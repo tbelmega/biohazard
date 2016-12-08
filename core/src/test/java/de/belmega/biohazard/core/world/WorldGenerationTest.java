b2b69267792e7b042dade5d89a3470068b6b913a
t.belmega@gmx.de <7B9deeyyUpjmqwNnX1Ej>
2016-12-08 23:15:23 +0100

    Improve world generation

--- core/src/test/java/de/belmega/biohazard/core/world/WorldGenerationTest.java
+++ core/src/test/java/de/belmega/biohazard/core/world/WorldGenerationTest.java
@@ -93,7 +93,7 @@ public class WorldGenerationTest {
         Country country = new WorldGenerator(params).generateCountry();
 
         //assert
-        assertThat((int) country.getPopulation(),
-                is(allOf(greaterThanOrEqualTo(90000), lessThanOrEqualTo(1600000000))));//TODO fix assertion. must not cast to int
+        assertThat(country.getPopulation(),
+                is(allOf(greaterThanOrEqualTo(90000L), lessThanOrEqualTo(1600000000L))));
     }
 }
